2486b629b81b68cddf8432fe04ef668ffa3c02a8
Trikita73 <diachenkoandrii7@gmail.com>
2025-08-01 16:01:27 +0300

    loader

--- js/main.js
+++ js/main.js
@@ -115,4 +115,23 @@ mainHeader.addEventListener('mousemove', (e) => {
 window.addEventListener('load', placeCards)
 window.addEventListener('resize', placeCards)
 
+window.addEventListener('load', function () {
+    const loaderInner = document.querySelector('.loader_inner');
+    const loader = document.querySelector('.loader');
+
+        // Плавное скрытие .loader_inner
+        loaderInner.style.opacity = '0';
+
+        // После окончания анимации (примерно 300 мс), скрываем весь preloader
+        setTimeout(() => {
+            loader.style.transition = 'opacity 0.6s ease';
+            loader.style.opacity = '0';
+
+        // Полностью убираем элемент из DOM через 600 мс
+        this.setTimeout(() => {
+            loader.style.display = 'none';
+        }, 600);
+    }, 400);
+});
+
 
